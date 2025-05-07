11242845c6dd3463a41081941c13a1190e27d587
FelipeLM000 <felipelaurindomartins@gmail.com>
2025-05-07 09:59:18 -0300

    sla

--- tads23_student-main/lib/main.dart
+++ tads23_student-main/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'common/config/dependencies.dart';
 import 'package:tads23_student/common/themes/themes.dart';
 import 'package:tads23_student/ui/pages/home_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      theme: myTheme,
-    ));
+  setupDependencies();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: const HomePage(),
+    theme: myTheme,
+  ));
 }
